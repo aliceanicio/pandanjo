a1837c3c95292257786e5badf49da63cf8f6ea4c
Alice <aliceanicio@hotmail.com>
2024-03-01 17:25:28 -0300

    feat: criação de componente de página de detalhes dos produtos

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailPageComponent } from './components/detail-page/detail-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailPageComponent
   ],
   imports: [
     BrowserModule,
