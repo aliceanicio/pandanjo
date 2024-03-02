20fd957ca818fc35c1b7438780efc06a216b82a8
Alice <aliceanicio@hotmail.com>
2024-03-02 16:16:17 -0300

    feat: criação de branch e componentes novas

---
+++ projeto/src/app/components/size-button/size-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-size-button',
+  templateUrl: './size-button.component.html',
+  styleUrl: './size-button.component.css'
+})
+export class SizeButtonComponent {
+
+}
